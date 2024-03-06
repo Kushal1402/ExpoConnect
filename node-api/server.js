bae8f5404163f90b4415759a47e7cf16a4cbd8cb
Kushal <Kushalhemant2003@gmail.com>
2024-03-06 09:13:35 +0530

    server file changes

--- node-api/server.js
+++ node-api/server.js
@@ -3,6 +3,8 @@ const app = require('./app')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
-console.log('Server started at : ' + port)
-const server = http.createServer(app)
-server.listen(port)
+// console.log('Server started at : ' + port)
+// const server = http.createServer(app)
+app.listen(port, () => {
+    console.log('Server started at : ' + port)
+})
